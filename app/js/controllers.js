c9218e2c6de81194ddd371ab1212a36465abcbdd
Rudy Rigot <rudy.rigot@gmail.com>
2014-05-17 22:37:22 -0700

    Save and init get deported to a service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,18 +2,10 @@
 
 /* Controllers */
 
-angular.module('klasroom.controllers', ['ngCookies'])
-  .controller('HomeController', ['$scope', '$cookieStore', function($scope, $cookieStore) {
+angular.module('klasroom.controllers', [])
+  .controller('HomeController', ['$scope', 'save', 'init', function($scope, save, init) {
 
-	$scope.c1Name = $cookieStore.get('c1Name') || 'Academic ability',
-	$scope.c2Name = $cookieStore.get('c2Name') || 'English-speaking level',
-	$scope.c3Name = $cookieStore.get('c3Name') || 'Willingness to help others',
-	$scope.c4Name = $cookieStore.get('c4Name') || 'Another criterium?',
-	$scope.nbTables = $cookieStore.get('nbTables') || '',
-
-	$scope.students = $cookieStore.get('students') || [];
-
-	$scope.tables = [];
+	init($scope);
 
 	$scope.addStudent = function() {
 		if($scope.newStudentName) {
@@ -34,13 +26,7 @@ angular.module('klasroom.controllers', ['ngCookies'])
 	}
 
 	$scope.save = function() {
-		$cookieStore.put('c1Name', $scope.c1Name);
-		$cookieStore.put('c2Name', $scope.c2Name);
-		$cookieStore.put('c3Name', $scope.c3Name);
-		$cookieStore.put('c4Name', $scope.c4Name);
-		$cookieStore.put('nbTables', $scope.nbTables);
-		$cookieStore.put('students', $scope.students);
-		alert("Your data is saved! You'll find it back in this browser until you hit save again.");
+		save($scope);
 	}
 
 	$scope.resetAll = function() {
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,29 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('klasroom.services', []).
-  value('version', '0.1');
+angular.module('klasroom.services', ['ngCookies']).
+  value('version', '0.1').
+  factory('save', ['$cookieStore', function($cookieStore){
+  	return function($scope) {
+		$cookieStore.put('c1Name', $scope.c1Name);
+		$cookieStore.put('c2Name', $scope.c2Name);
+		$cookieStore.put('c3Name', $scope.c3Name);
+		$cookieStore.put('c4Name', $scope.c4Name);
+		$cookieStore.put('nbTables', $scope.nbTables);
+		$cookieStore.put('students', $scope.students);
+		alert("Your data is saved! You'll find it back in this browser until you hit save again.");
+  	}
+  }]).
+  factory('init', ['$cookieStore', function($cookieStore){
+  	return function($scope) {
+		$scope.c1Name = $cookieStore.get('c1Name') || 'Academic ability',
+		$scope.c2Name = $cookieStore.get('c2Name') || 'English-speaking level',
+		$scope.c3Name = $cookieStore.get('c3Name') || 'Willingness to help others',
+		$scope.c4Name = $cookieStore.get('c4Name') || 'Another criterium?',
+		$scope.nbTables = $cookieStore.get('nbTables') || '',
+
+		$scope.students = $cookieStore.get('students') || [];
+
+		$scope.tables = [];
+  	}
+  }]);
